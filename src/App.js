09c6a0fd2acf53aa47a7b2ea796a5c02cdab9667
luisCoder18 <luis121895@gmail.com>
2024-08-06 19:31:33 -0500

    Mensaje del commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <TodoCounter/>
+     <TodoSearch/>
+     
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton/>
+    </div>
+  );
+}
+
+
+function TodoCounter() {
+  return (
+    <h1>
+        Has completado 3 de 5 TODOS
+    </h1>
+  );
+}
+function TodoSearch() {
+  return (
+    <h1>
+        Has completado 3 de 5 TODOS
+    </h1>
+  );
+}
+function TodoList() {
+  return (
+    <h1>
+        Has completado 3 de 5 TODOS
+    </h1>
+  );
+}
+function CreateTodoButton() {
+  return (
+    <h1>
+        Has completado 3 de 5 TODOS
+    </h1>
+  );
+}
+
+
+function TodoItem() {      //componente
+  return (
+    //elemento jsx lo pasan por babel y lo convierte a js
+    <li>
+      <span>V</span>
+      <p>Lorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export default App;
